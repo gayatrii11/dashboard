ef4a6250e8aa49e10096b458868564b6e84cd9ac
Akshay <60477442+akshayitzme@users.noreply.github.com>
2022-01-29 17:35:04 +0530

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'akshay'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'dashboard_app-dev'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def home():
+    return "<p>Hello, World!</p>"
+
+@app.route("/dashboard")
+def dashboard():
+    df = pd.DataFrame({
+      'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
+      'Amount': [4, 1, 2, 2, 4, 5],
+      'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    })   
+    fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')   
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("dashboard.html", graphJSON= graphJSON )
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Setup
+
+- First Install Necessary Libraries / Dependancies
+    ```pip install -r requirements.txt```
+
+- Running the app
+    in the cmd/ powershell ``` flask run ```
+
+    
